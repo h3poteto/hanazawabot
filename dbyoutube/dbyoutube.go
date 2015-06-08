37e1010bd314d7654a0a0587cc82fa20e779a74e
h3poteto <h3.poteto@gmail.com>
2015-06-09 00:54:53 +0900

    tweetタスクの追加

--- dbyoutube/dbyoutube.go
+++ dbyoutube/dbyoutube.go
@@ -10,6 +10,8 @@ import (
 
 type YoutubeMovie interface {
 	Add(string, string, string) bool
+	SelectRandom() (tweet string, error string)
+	convertYoutubeID(string) string
 }
 
 type DBYoutubeMovie struct {
@@ -27,7 +29,35 @@ func (u *DBYoutubeMovie) Add(title string, id string, description string) bool {
 		fmt.Printf("mysql connect error: %v \n", err)
 	}
 
-	db.Close()
+	defer db.Close()
 
 	return true
+}
+
+func (u *DBYoutubeMovie) SelectRandom() (tweet string, error string) {
+	db, err := sql.Open("mysql", "root:@/hanazawa?charset=utf8")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rows, err := db.Query("select * from youtube_movies order by rand() limit 1;")
+	if err != nil {
+		fmt.Printf("mysql connect error: %v \n", err)
+	}
+
+	defer db.Close()
+
+	id, movie_id, title, description, disabled, created_at, updated_at := 0, "", "", "", 0 , "", ""
+	for rows.Next() {
+		err = rows.Scan(&id, &title, &movie_id, &description, &disabled, &created_at, &updated_at)
+		if err != nil{
+			return "", err.Error()
+		}
+	}
+
+	return u.convertYoutubeID(movie_id), ""
+}
+
+func (u *DBYoutubeMovie) convertYoutubeID(movie_id string) string {
+	return "https://www.youtube.com/watch?v=" + movie_id
 }
--- tweet.go
+++ tweet.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+
+	"github.com/ChimeraCoder/anaconda"
+
+	"./dbyoutube"
+)
+
+func main() {
+	anaconda.SetConsumerKey(os.Getenv("TWITTER_CONSUMER_KEY"))
+	anaconda.SetConsumerSecret(os.Getenv("TWITTER_CONSUMER_SECRET"))
+	api := anaconda.NewTwitterApi(os.Getenv("TWITTER_OAUTH_TOKEN"), os.Getenv("TWITTER_OAUTH_SECRET"))
+
+
+	myDb := &dbyoutube.DBYoutubeMovie{}
+	var db dbyoutube.YoutubeMovie = myDb
+
+	youtube_url, err := db.SelectRandom()
+	if err != "" {
+		log.Fatalf("DBYoutube random select error: %v", err)
+	}
+	fmt.Printf("%v \n", youtube_url)
+
+	_, error := api.PostTweet("わーい" + youtube_url, nil)
+	if error != nil {
+		log.Fatalf("twitter api error: %v", error)
+	}
+}
