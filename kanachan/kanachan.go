b76386c82100b5359ded5e677815a04e331b21f1
h3poteto <h3.poteto@gmail.com>
2015-06-14 00:23:52 +0900

    Userstreamの初期タスクを追加

--- kanachan/kanachan.go
+++ kanachan/kanachan.go
@@ -6,10 +6,12 @@ import (
 
 var (
 	except_words = [...]string{"歌ってみた", "踊ってみた"}
+	include_words = [...]string{"花澤 香菜", "花澤香菜", "花澤"}
 )
 
 type Kana interface {
 	ExceptCheck(string) bool
+	IncludeCheck(string) bool
 }
 
 type Kanachan struct {
@@ -22,4 +24,14 @@ func (u *Kanachan) ExceptCheck(word string) bool {
 		}
 	}
 	return true
+}
+
+
+func (u *Kanachan) IncludeCheck(word string) bool {
+	for _, include := range include_words {
+		if strings.Contains(word, include) {
+			return true
+		}
+	}
+	return false
 }
---
+++ userstream.go
@@ -0,0 +1,78 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"net/url"
+	"strings"
+
+	"github.com/ChimeraCoder/anaconda"
+	"./kanachan"
+	"./dbyoutube"
+
+)
+
+func main() {
+	anaconda.SetConsumerKey(os.Getenv("TWITTER_CONSUMER_KEY"))
+	anaconda.SetConsumerSecret(os.Getenv("TWITTER_CONSUMER_SECRET"))
+	api := anaconda.NewTwitterApi(os.Getenv("TWITTER_OAUTH_TOKEN"), os.Getenv("TWITTER_OAUTH_SECRET"))
+
+	values := url.Values{}
+	self, _ := api.GetSelf(values)
+	stream := api.UserStream(values)
+	for {
+		event := <-stream.C
+		switch event.(type) {
+		case anaconda.Tweet:
+			tweet := event.(anaconda.Tweet)
+
+			ydb := &dbyoutube.DBYoutubeMovie{}
+			var youtubedb dbyoutube.YoutubeMovie = ydb
+
+
+			aKana := &kanachan.Kanachan{}
+			var kana kanachan.Kana = aKana
+			if kana.IncludeCheck(tweet.Text) {
+				// DB保存
+			}
+			// RTチェック
+			// TODO: リプライチェック
+			if checkRetweet(tweet, self) {
+			} else if checkReply(tweet, self) {
+				youtube_url, err := youtubedb.SelectRandom()
+				if err != "" {
+					fmt.Printf("DBYoutube random select error: %v", err)
+					continue
+				}
+				tweet_value := url.Values{}
+				tweet_value.Set("in_reply_to_status_id", tweet.IdStr)
+				_, error := api.PostTweet("@" + tweet.User.ScreenName + " " + youtube_url, tweet_value)
+				if error != nil {
+					fmt.Printf("twitter api error: %v", error)
+				}
+
+			} else {
+				continue
+			}
+			// favチェック
+		}
+	}
+}
+
+func checkReply(tweet anaconda.Tweet, user anaconda.User) bool {
+	if tweet.InReplyToUserID == user.Id {
+		return true
+	}
+	if strings.Contains(tweet.Text, "@" + user.ScreenName) {
+		return true
+	}
+	return false
+}
+
+func checkRetweet(tweet anaconda.Tweet, user anaconda.User) bool {
+	if strings.Index(tweet.Text, "RT") == 0 && tweet.User.Id != user.Id && strings.Contains(tweet.Text, "@" + user.ScreenName) {
+		return true
+	} else {
+		return false
+	}
+}
