b150c028980ea92d82735b2dd12014234748f2a5
h3poteto <h3.poteto@gmail.com>
2015-07-12 23:35:50 +0900

    refs #12 kanachanのテストを導入

--- kanachan/kanachan_test.go
+++ kanachan/kanachan_test.go
@@ -1,55 +1,61 @@
 package kanachan
 
 import (
-	"strings"
-	"unicode/utf8"
+	"testing"
+	"."
 )
 
-var (
-	except_words = [...]string{"歌ってみた", "踊ってみた"}
-	include_words = [...]string{"花澤 香菜", "花澤香菜", "花澤"}
-)
+func TestExceptCheck(t *testing.T) {
+	aKana := &kanachan.Kanachan{}
+	var kana kanachan.Kana = aKana
 
-type Kana interface {
-	ExceptCheck(string) bool
-	IncludeCheck(string) bool
-	BuildTweet(string, string, string, string) string
-}
-
-type Kanachan struct {
-}
-
-func (u *Kanachan) ExceptCheck(word string) bool {
-	for _, except := range except_words {
-		if strings.Contains(word, except) {
-			return false
-		}
+	if kana.ExceptCheck("花澤香菜 歌ってみた") != false {
+		t.Error("歌ってみたを含むワードが除外されない")
 	}
-	return true
+
+	if kana.ExceptCheck("花澤香菜踊ってみた") != false {
+		t.Error("踊ってみたを含むワードが除外されない")
+	}
 }
 
+func TestIncludeCheck(t *testing.T) {
+	aKana := &kanachan.Kanachan{}
+	var kana kanachan.Kana = aKana
 
-func (u *Kanachan) IncludeCheck(word string) bool {
-	for _, include := range include_words {
-		if strings.Contains(word, include) {
-			return true
-		}
+	if kana.IncludeCheck("撫子ちゃん，花澤さん") != true {
+		t.Error("花澤というワードに反応していない")
 	}
-	return false
+
+	if kana.IncludeCheck("撫子ちゃん，花澤香菜") != true {
+		t.Error("花澤香菜というワードに反応していない")
+	}
+
+	if kana.IncludeCheck("撫子ちゃん，花澤 香菜") != true {
+		t.Error("花澤 香菜というワードに反応していない")
+	}
+
+	if kana.IncludeCheck("撫子ちゃん，花澤　香菜") != true {
+		t.Error("花澤　香菜というワードに反応していない")
+	}
+
+	if kana.IncludeCheck("なでこちゃん") != false {
+		t.Error("関係ないワードに反応している")
+	}
 }
 
-func (u *Kanachan) BuildTweet(reply string, serif string, title string, url string) string {
-	tweet := reply + serif + " " + "【" + title + "】"
-	// twitterはURLを短縮するので，URLは必ず23文字以下になる
-	// http://tech.aainc.co.jp/archives/6472
-	over_count := utf8.RuneCountInString(tweet) - (140 - 25)
-	if over_count > 0 {
-		title_length := utf8.RuneCountInString(title)
-		r := []rune(title)
-		new_title := string(r[0:(title_length - over_count)])
-		tweet = reply + serif + " " + "【" + new_title + "…】" + url
-	} else {
-		tweet += url
+func TestBuildTweet(t *testing.T) {
+	aKana := &kanachan.Kanachan{}
+	var kana kanachan.Kana = aKana
+
+	if kana.BuildTweet("", "台詞", "タイトル", "https://youtube.com/") != "台詞 【タイトル】https://youtube.com/" {
+		t.Error("リプライなしのツイート文字列生成が上手く行ってない")
 	}
-	return tweet
+
+	if kana.BuildTweet("@h3_poteto ", "台詞", "タイトル", "https://youtube.com/") != "@h3_poteto 台詞 【タイトル】https://youtube.com/" {
+		t.Error("リプライのツイート文字列生成が上手く行ってない")
+	}
+
+	if kana.BuildTweet("@h3_poteto ", "あたしもう25歳だよ！お前あたしと結婚できんのか！", "【責任取れ！】花澤香菜「あたしもう25歳だよ！お前あたしと結婚できんのか！」豊永利行「責任取ればいいの？///」杉田智和の好き勝手な発言に花澤香菜が絶叫www「花澤さんがおもいっきり吹いたこの紙笛はこちらの宛先まで♪」www", "https://youtube.com/") != "@h3_poteto あたしもう25歳だよ！お前あたしと結婚できんのか！ 【【責任取れ！】花澤香菜「あたしもう25歳だよ！お前あたしと結婚できんのか！」豊永利行「責任取ればいいの？///」杉田智和の好き勝手な発言に花澤香菜が絶叫…】https://youtube.com/" {
+		t.Error("140字を超えるツイートを短縮できていない")
+	}
 }
